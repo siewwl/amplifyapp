192bd2b71048bb2399605328a1e04c1810fa6e00
unknown <mypropertytoolssg@gmail.com>
2021-09-16 14:37:33 +0800

    commit 7

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
+
+// ***************** Code before Auth *****************************
+/* 
 import logo from './logo.svg';
 import './App.css';
 
@@ -28,3 +49,4 @@ function App() {
 }
 
 export default App;
+*/
